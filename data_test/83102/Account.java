20e8c30a1a3ef71f739fe2f9990fd1e37825091c
Huyen1204 <nguyenhuyen98pm@gmail.com>
2021-07-02 10:51:39 +0700

    temp

---
+++ data_test/83102/Account.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Account {
+    public static final String CHECKING = "CHECKING";
+    public static final String SAVINGS = "SAVINGS";
+
+    private long accountNumber;
+    private double balance;
+    protected List<Transaction> transactionList = new ArrayList<>();
+
+    public Account() {
+    }
+
+    public Account(long accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public abstract void deposit(double amount);
+
+    public abstract void withdraw(double amount);
+
+    /**
+     * javadoc.
+     */
+    public void doDepositing(double amount) throws InvalidFundingAmountException {
+        if (amount < 0) {
+            throw new InvalidFundingAmountException(amount);
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void doWithdrawing(double amount)
+    throws InvalidFundingAmountException, InsufficientFundsException {
+        if (amount < 0) {
+            throw new InvalidFundingAmountException(amount);
+        } else if (amount > balance) {
+            throw new InsufficientFundsException(amount);
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactionList.add(transaction);
+    }
+
+    /**
+     * javadoc.
+     */
+    public String getTransactionHistory() {
+        StringBuilder res = new StringBuilder();
+        res.append("Lịch sử giao dịch của tài khoản ");
+        res.append(accountNumber);
+        res.append(":" + "\n");
+
+        for (Transaction transaction : transactionList) {
+            res.append(transaction.getTransactionSummary()).append("\n");
+        }
+        //res.deleteCharAt(res.lastIndexOf("\n"));
+        return res.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Account account = (Account) o;
+
+        return accountNumber == account.accountNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (accountNumber ^ (accountNumber >>> 32));
+    }
+}
+ 
---
+++ data_test/83102/Transaction.java
@@ -0,0 +1,51 @@
+import java.text.DecimalFormat;
+
+/**
+ * Created by CCNE on 03/12/2020.
+ */
+public class Transaction {
+    public static final int TYPE_DEPOSIT_CHECKING = 1;
+    public static final int TYPE_WITHDRAW_CHECKING = 2;
+    public static final int TYPE_DEPOSIT_SAVINGS = 3;
+    public static final int TYPE_WITHDRAW_SAVINGS = 4;
+
+    private int type;
+    private double amount;
+    private double initialBalance;
+    private double finalBalance;
+
+    /**
+     * javadoc.
+     */
+    public Transaction(int type, double amount, double initialBalance, double finalBalance) {
+        this.type = type;
+        this.amount = amount;
+        this.initialBalance = initialBalance;
+        this.finalBalance = finalBalance;
+    }
+
+    private String getTransactionTypeString(int type) {
+        //"Nạp tiền vãng lai", "Rút tiền vãng lai", "Nạp tiền tiết kiệm", "Rút tiền tiết kiệm"
+        if (type == 1) {
+            return "Nạp tiền vãng lai";
+        } else if (type == 2) {
+            return "Rút tiền vãng lai";
+        } else if (type == 3) {
+            return "Nạp tiền tiết kiệm";
+        } else {
+            return "Rút tiền tiết kiệm";
+        }
+    }
+
+    /**
+     * javadoc.
+     */
+    public String getTransactionSummary() {
+        DecimalFormat df2 = new DecimalFormat("0.00");
+        return "- Kiểu giao dịch: " + getTransactionTypeString(type)
+                + ". Số dư ban đầu: " + df2.format(initialBalance)
+                + ". Số tiền: $" + df2.format(amount)
+                + ". Số dư cuối: $" + df2.format(finalBalance) + ".";
+    }
+}
+ 
